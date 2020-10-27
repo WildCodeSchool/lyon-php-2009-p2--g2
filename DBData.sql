ee9a30a9b2949534822db63db8dbb80ddab89fa9
Yoann PARDO-VINCENT <65440660+Yoyaug@users.noreply.github.com>
2020-10-27 22:41:40 +0100

    Add AUTO_INCREMENT to all id in tables. DELETE DBTableCreation.sql and REPLACE it by DBCreation.sql sql file to implement some fake data inside database (DBData.sql)

---
+++ DBData.sql
@@ -0,0 +1,103 @@
+-- -----------------------------------------------------
+-- Insert some data inside our tables (DB : office_warriors)
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Table user
+-- -----------------------------------------------------
+
+INSERT INTO user (
+    username,
+    email,
+    password,
+    is_admin,
+    is_logged
+  ) VALUES (
+    "admin",
+    "admin@admin.admin",
+    "admin",
+    "1",
+    "1"
+  );
+
+-- -----------------------------------------------------
+-- Table `item`
+-- -----------------------------------------------------
+
+INSERT INTO item (
+    name,
+    image,
+    strength,
+    energy,
+    humor,
+    agility
+  ) VALUES (
+    "tasse à café",
+    "https://via.placeholder.com/150",
+    "1",
+    "2",
+    "0",
+    "-1"
+  );
+
+-- -----------------------------------------------------
+-- Table `event`
+-- -----------------------------------------------------
+
+INSERT INTO event (
+    name,
+    image,
+    strength,
+    energy,
+    humor,
+    agility,
+    floor_restriction
+  ) VALUES (
+    "Event 0",
+    "https://via.placeholder.com/150",
+    "0",
+    "0",
+    "0",
+    "0",
+    "1" 
+  ), (
+    "Event 1 hard NRJ",
+    "https://via.placeholder.com/150",
+    "2",
+    "10",
+    "0",
+    "1",
+    "1"  
+  ), (
+    "Event 2",
+    "https://via.placeholder.com/150",
+    "2",
+    "2",
+    "2",
+    "2",
+    "1"  
+  ), (
+    "Event 3",
+    "https://via.placeholder.com/150",
+    "2",
+    "3",
+    "4",
+    "5",
+    "2"  
+  ), (
+    "Event 4 EZ",
+    "https://via.placeholder.com/150",
+    "0",
+    "0",
+    "0",
+    "0",
+    "2"  
+  ), (
+    "Event 5 EZ NRJ",
+    "https://via.placeholder.com/150",
+    "10",
+    "0",
+    "10",
+    "10",
+    "2"  
+  );
