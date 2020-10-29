d7b8bb94cd3e13d5eb9691fe11c84b91762fda10
yoann <yoann@LAPTOP-5JKAFLLK.localdomain>
2020-10-29 22:03:06 +0100

    Creation of the GameController, CharacterManager and character.html.twig

---
+++ src/Controller/GameController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 11/10/17
+ * Time: 16:07
+ * PHP version 7
+ */
+
+namespace App\Controller;
+
+use App\Model\CharacterManager;
+
+/**
+ * Class CharacterController
+ *
+ */
+class CharacterController extends AbstractController
+{
+
+    public function index()
+    {
+        return $this->twig->render('Character/character.html.twig');
+    }
+
+}
---
+++ src/Model/CharacterManager.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sylvain
+ * Date: 07/03/18
+ * Time: 18:20
+ * PHP version 7
+ */
+
+namespace App\Model;
+
+/**
+ *
+ */
+class CharacterManager extends AbstractManager
+{
+    /**
+     *
+     */
+    const TABLE = 'office_warriors';
+
+
+}
