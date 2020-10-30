b9f9fbce26d74189d527915884f899cddef5ba3f
raquellaur <raquel.navega@gmail.com>
2020-10-30 08:45:49 +0100

    creation new structure

---
+++ src/Model/EventManager.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Model;
+
+class EventManager extends AbstractManager
+{
+    /**
+     *
+     */
+    const TABLE = 'event';
+
+    /**
+     *  Initializes this class.
+     */
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+
+}
