50324ed1abd3939d47ac3e8d7053d9b2cd0c76e6
yoann <yoann@LAPTOP-5JKAFLLK.localdomain>
2020-10-30 16:25:32 +0100

    Better DB, div for the view, script js create, stat.html.twig for the stats setting

--- DBCreation.sql
+++ DBCreation.sql
@@ -10,6 +10,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Database creation - Office Warriors
 -- -----------------------------------------------------
+DROP DATABASE `office_warriors`;
 CREATE DATABASE IF NOT EXISTS `office_warriors`;
 USE `office_warriors` ;
 
