98e4771d9e8afb40ef03b21913dea2ed63e14fad
yoann <yoann@LAPTOP-5JKAFLLK.localdomain>
2020-11-10 13:46:37 +0100

    Base View for elevator, first method in GameController

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -38,7 +38,7 @@ class GameController extends AbstractController
      */
     public function createCharacter()
     {
-        return $this->twig->render('Character/character.html.twig');
+        return $this->twig->render ( 'Character/character.html.twig' );
     }
 
     /**
@@ -54,9 +54,9 @@ class GameController extends AbstractController
             'jpeg' => 'image/jpeg',
             'jpg' => 'image/jpeg',
         ];
-        if (isset($_FILES) && $_SERVER['REQUEST_METHOD'] === 'POST') {
-            if (isset($_POST['name']) && !empty($_POST['name'])) {
-                $name = trim($_POST['name']);
+        if (isset( $_FILES ) && $_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (isset( $_POST['name'] ) && !empty( $_POST['name'] )) {
+                $name = trim ( $_POST['name'] );
             } else {
                 $errors['type'] = "Sorry mate, you need to enter a name for your character !";
             }
@@ -67,7 +67,7 @@ class GameController extends AbstractController
                 $errors['size'] = "Sorry mate, your file is too big";
             }
             if (!empty( $errors )) {
-                return $this->twig->render('Character/character.html.twig', ['errors' => $errors]);
+                return $this->twig->render ( 'Character/character.html.twig', ['errors' => $errors] );
             } else {
                 $extension = pathinfo ( $_FILES['image']['name'], PATHINFO_EXTENSION );
                 $filename = uniqid () . '.' . $extension;
@@ -83,52 +83,59 @@ class GameController extends AbstractController
                     'humor' => $_POST['humor'],
                     'agility' => $_POST['agility'],
                 ];
-                $id = $gameManager->newGame($character);
-                header("Location:/game/event/$id");
+                $id = $gameManager->newGame ( $character );
+                header ( "Location:/game/event/$id" );
             }
         } else {
             $errors['emptyFile'] = "You need to upload an avatar in order to play";
-            return $this->twig->render('Character/character.html.twig', ['errors' => $errors]);
+            return $this->twig->render ( 'Character/character.html.twig', ['errors' => $errors] );
         }
     }
 
     public function event($idGame)
     {
         $game = new GameManager();
-        $newPlayer = $game->selectOneById($idGame);
-        if (!empty($newPlayer['max_floor'])) {
+        $newPlayer = $game->selectOneById ( $idGame );
+        if (!empty( $newPlayer['max_floor'] )) {
             //Take the game floor to know wich events put in array events.
             $floor = $newPlayer['max_floor'];
             $newEvent = new EventManager();
             //Take all events by floor
-            $this->events = $newEvent->selectAllEvents($floor);
+            $this->events = $newEvent->selectAllEvents ( $floor );
             //Take the events made by floor in game_has_event
             $newGameEvent = new GameEventManager();
 
-            $playerEvents = $newGameEvent->selectAllGameEvents($idGame);
+            $playerEvents = $newGameEvent->selectAllGameEvents ( $idGame );
             $arrayPlayerEvents = [];
             $arrayEvents = [];
             $chooseEvent = [];
-            if (count($playerEvents) === 0) {
-                $chooseEvent = $this->events[array_rand($this->events)];
-            } elseif (count($playerEvents) >= 1) {
+            if (count ( $playerEvents ) === 0) {
+                $chooseEvent = $this->events[array_rand ( $this->events )];
+            } elseif (count ( $playerEvents ) >= 1) {
                 foreach ($playerEvents as $playerEvent) {
                     $arrayPlayerEvents[] = $playerEvent['event_id'];
                 }
                 foreach ($this->events as $event) {
                     $arrayEvents[] = $event['id'];
                 }
-                $result = array_diff($arrayEvents, $arrayPlayerEvents);
-                $chooseEvent = $this->events[array_rand($result)];
+                $result = array_diff ( $arrayEvents, $arrayPlayerEvents );
+                $chooseEvent = $this->events[array_rand ( $result )];
             }
             $newItems = new ItemManager();
-            $itemsPlayer = $newItems->selectAllPlayerItems($idGame);
-            return $this->twig->render('Game/event.html.twig', [
+            $itemsPlayer = $newItems->selectAllPlayerItems ( $idGame );
+            return $this->twig->render ( 'Game/event.html.twig', [
                 'event' => $chooseEvent,
                 'player' => $newPlayer,
-                'items' => $itemsPlayer]);
+                'items' => $itemsPlayer] );
         } else {
             echo 'Character doesnt exist!';
         }
-    }    
+    }
+
+    public function elevator($idGame)
+    {
+        $game = new GameManager();
+        $newPlayer = $game->selectOneById ( $idGame );
+        return $this->twig->render('Elevator/elevator.html.twig', ['player' => $newPlayer]);
+    }
 }
